843d0ec649c631f71999b2307a23a1b0cade313e
Adrian <adry.dabp@gmail.com>
2018-09-09 13:49:59 -0400

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import routes from './src/routes';
+console.log(routes);
+module.exports = {
+  title: 'Hello desde vuepress',
+  description: 'Aqui hay una descripcion para vuepress',
+  themeConfig: routes
+};
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+export default {
+  routes: [
+    {
+      text: 'home',
+      link: '/'
+    },
+    {
+      text: 'options',
+      items: [
+        {
+          text: 'options 1',
+          link: '/options1'
+        },
+        {
+          text: 'options 2',
+          link: '/options2'
+        },
+      ]
+    }
+  ]
+};
